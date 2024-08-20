641b8e9ec653891d0c113acc686a4b41e13725af
Caio Meletti <meletti@gmail.com>
2024-08-19 23:17:14 -0300

    ✨ feat: versão Winforms Primeira versão do aplicativo Revisado por: Caio M

---
+++ v0/AmigoSecreto/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace AmigoSecreto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkEmbaralhar = new System.Windows.Forms.CheckBox();
+            this.chkExibir = new System.Windows.Forms.CheckBox();
+            this.cmdReiniciar = new System.Windows.Forms.Button();
+            this.cmdSortear = new System.Windows.Forms.Button();
+            this.lstParticipantes = new System.Windows.Forms.ListBox();
+            this.cmdCarregar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // chkEmbaralhar
+            // 
+            this.chkEmbaralhar.AutoSize = true;
+            this.chkEmbaralhar.Location = new System.Drawing.Point(188, 167);
+            this.chkEmbaralhar.Name = "chkEmbaralhar";
+            this.chkEmbaralhar.Size = new System.Drawing.Size(79, 17);
+            this.chkEmbaralhar.TabIndex = 10;
+            this.chkEmbaralhar.Text = "Embaralhar";
+            this.chkEmbaralhar.UseVisualStyleBackColor = true;
+            // 
+            // chkExibir
+            // 
+            this.chkExibir.AutoSize = true;
+            this.chkExibir.Location = new System.Drawing.Point(188, 144);
+            this.chkExibir.Name = "chkExibir";
+            this.chkExibir.Size = new System.Drawing.Size(101, 17);
+            this.chkExibir.TabIndex = 8;
+            this.chkExibir.Text = "Exibir ao sortear";
+            this.chkExibir.UseVisualStyleBackColor = true;
+            // 
+            // cmdReiniciar
+            // 
+            this.cmdReiniciar.Enabled = false;
+            this.cmdReiniciar.Location = new System.Drawing.Point(188, 94);
+            this.cmdReiniciar.Name = "cmdReiniciar";
+            this.cmdReiniciar.Size = new System.Drawing.Size(101, 23);
+            this.cmdReiniciar.TabIndex = 9;
+            this.cmdReiniciar.Text = "Reiniciar";
+            this.cmdReiniciar.UseVisualStyleBackColor = true;
+            this.cmdReiniciar.Click += new System.EventHandler(this.cmdReiniciar_Click);
+            // 
+            // cmdSortear
+            // 
+            this.cmdSortear.Enabled = false;
+            this.cmdSortear.Location = new System.Drawing.Point(188, 65);
+            this.cmdSortear.Name = "cmdSortear";
+            this.cmdSortear.Size = new System.Drawing.Size(101, 23);
+            this.cmdSortear.TabIndex = 7;
+            this.cmdSortear.Text = "Sortear";
+            this.cmdSortear.UseVisualStyleBackColor = true;
+            this.cmdSortear.Click += new System.EventHandler(this.cmdSortear_Click);
+            // 
+            // lstParticipantes
+            // 
+            this.lstParticipantes.FormattingEnabled = true;
+            this.lstParticipantes.Location = new System.Drawing.Point(12, 12);
+            this.lstParticipantes.Name = "lstParticipantes";
+            this.lstParticipantes.Size = new System.Drawing.Size(161, 225);
+            this.lstParticipantes.TabIndex = 6;
+            // 
+            // cmdCarregar
+            // 
+            this.cmdCarregar.Location = new System.Drawing.Point(188, 12);
+            this.cmdCarregar.Name = "cmdCarregar";
+            this.cmdCarregar.Size = new System.Drawing.Size(101, 23);
+            this.cmdCarregar.TabIndex = 6;
+            this.cmdCarregar.Text = "Carregar lista";
+            this.cmdCarregar.UseVisualStyleBackColor = true;
+            this.cmdCarregar.Click += new System.EventHandler(this.cmdCarregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 252);
+            this.Controls.Add(this.cmdCarregar);
+            this.Controls.Add(this.lstParticipantes);
+            this.Controls.Add(this.cmdSortear);
+            this.Controls.Add(this.cmdReiniciar);
+            this.Controls.Add(this.chkExibir);
+            this.Controls.Add(this.chkEmbaralhar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Amigo Secreto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox chkEmbaralhar;
+        private System.Windows.Forms.CheckBox chkExibir;
+        private System.Windows.Forms.Button cmdReiniciar;
+        private System.Windows.Forms.Button cmdSortear;
+        private System.Windows.Forms.ListBox lstParticipantes;
+        private System.Windows.Forms.Button cmdCarregar;
+    }
+}
+
---
+++ v0/AmigoSecreto/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AmigoSecreto
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList ListaParticipantes;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CarregaParticipantes()
+        {
+            string text = "";
+            ListaParticipantes = new ArrayList();
+            using (StreamReader streamReader = File.OpenText("Participantes.txt"))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    text = streamReader.ReadLine();
+                    ListaParticipantes.Add(text);
+                }
+                streamReader.Close();
+            }
+            if (chkEmbaralhar.Checked)
+            {
+                int count = ListaParticipantes.Count;
+                Random random = new Random();
+                for (int i = 0; i < ListaParticipantes.Count; i++)
+                {
+                    int index = random.Next(count);
+                    string value = ListaParticipantes[i].ToString();
+                    ListaParticipantes[i] = ListaParticipantes[index];
+                    ListaParticipantes[index] = value;
+                }
+            }
+            lstParticipantes.Items.Clear();
+            lstParticipantes.Items.AddRange(ListaParticipantes.ToArray());
+        }
+
+        private void cmdCarregar_Click(object sender, EventArgs e)
+        {
+            CarregaParticipantes();
+            cmdSortear.Enabled = true;
+            cmdCarregar.Enabled = false;
+        }
+
+        private void cmdReiniciar_Click(object sender, EventArgs e)
+        {
+            cmdReiniciar.Enabled = false;
+            cmdCarregar.Enabled = true;
+        }
+
+        private void cmdSortear_Click(object sender, EventArgs e)
+        {
+            cmdSortear.Enabled = false;
+            int count = lstParticipantes.Items.Count;
+            string[] array = new string[count];
+            string[] array2 = new string[count];
+            int i = 0;
+            string text = "Sorteio" + DateTime.Now.ToString("yyyyMMdd-HHmmss");
+            for (int j = 0; j < count; j++)
+            {
+                array[j] = lstParticipantes.Items[j].ToString();
+            }
+            Random random = new Random();
+            for (; i < count; i++)
+            {
+                int num = random.Next(count);
+                while (lstParticipantes.Items[num].Equals("*") || array[i].Equals(array[num]))
+                {
+                    num = random.Next(count);
+                }
+                array2[i] = array[num];
+                if (chkExibir.Checked)
+                {
+                    MessageBox.Show(array[i].ToString() + " -> " + array2[i].ToString());
+                }
+                Directory.CreateDirectory(text);
+                StreamWriter streamWriter = File.CreateText(Path.Combine(text, array[i].ToString() + ".txt"));
+                streamWriter.Write(array2[i].ToString());
+                streamWriter.Close();
+                lstParticipantes.Items[num] = "*";
+            }
+            cmdReiniciar.Enabled = true;
+        }
+    }
+}
